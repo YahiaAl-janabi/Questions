ffb50e14af1f88a0d811cd41f0638dd6648b282a
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2022-01-16 14:51:19 +0000

    added some documentation

--- script.js
+++ script.js
@@ -12,16 +12,28 @@
 //   });
 // });
 
+// Selects all the question elements
+
 const questions = document.querySelectorAll('.question');
 
+// Iterates over every question element
+
 questions.forEach(function (question) {
+  // constants for every element inside the current question element
+
   const btn = question.querySelector('.questionBtn');
   const plusBtn = question.querySelector('.plusBtn');
   const minusBtn = question.querySelector('.minusBtn');
   const questionText = question.querySelector('.questionText');
 
+  // Add's an click event listener to the button element inside of the current question element
+
   btn.addEventListener('click', function () {
+    // iterate over the question elements again
+
     questions.forEach(function (item) {
+      // If the question element that is been iterated over again is not the same as the current question then remove the following classes
+
       if (item.querySelector('.questionText') !== questionText) {
         item
           .querySelector('.questionText')
@@ -30,6 +42,8 @@ questions.forEach(function (question) {
         item.querySelector('.minusBtn').classList.remove('minusBtnInactive');
       }
     });
+
+    // Once the current question's button is clicked it will then toggle the following classes
 
     plusBtn.classList.toggle('plusBtnInactive');
     minusBtn.classList.toggle('minusBtnInactive');
